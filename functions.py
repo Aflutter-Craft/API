56eea551193a5785ec5194ca6223117e8d5c05ef
blacksuan19 <abubakaryagob@gmail.com>
2021-05-07 14:24:39 +0800

    main: split to separate components
    Signed-off-by: blacksuan19 <i@blacksuan19.me>

---
+++ functions.py
@@ -0,0 +1,55 @@
+import torch.nn as nn
+from torchvision import transforms
+
+# loss functions
+def calc_mean_std(feat, eps=1e-5):
+    # eps is a small value added to the variance to avoid divide-by-zero.
+    size = feat.size()
+    assert (len(size) == 4)
+    N, C = size[:2]
+    feat_var = feat.view(N, C, -1).var(dim=2) + eps
+    feat_std = feat_var.sqrt().view(N, C, 1, 1)
+    feat_mean = feat.view(N, C, -1).mean(dim=2).view(N, C, 1, 1)
+    return feat_mean, feat_std
+
+
+def mean_variance_norm(feat):
+    size = feat.size()
+    mean, std = calc_mean_std(feat)
+    normalized_feat = (feat - mean.expand(size)) / std.expand(size)
+    return normalized_feat
+
+
+# transforms to perfom on image before using
+def test_transform():
+    transform_list = []
+    transform_list.append(transforms.Resize(512))
+    transform_list.append(transforms.ToTensor())
+    transform = transforms.Compose(transform_list)
+    return transform
+
+
+# extract style and content features
+def feat_extractor(vgg, content, style, DEVICE):
+    # extract used layers from vgg network
+    enc_1 = nn.Sequential(*list(vgg.children())[:4])  # input -> relu1_1
+    enc_2 = nn.Sequential(*list(vgg.children())[4:11])  # relu1_1 -> relu2_1
+    enc_3 = nn.Sequential(*list(vgg.children())[11:18])  # relu2_1 -> relu3_1
+    enc_4 = nn.Sequential(*list(vgg.children())[18:31])  # relu3_1 -> relu4_1
+    enc_5 = nn.Sequential(*list(vgg.children())[31:44])  # relu4_1 -> relu5_1
+
+    # move everything to GPU
+    enc_1.to(DEVICE)
+    enc_2.to(DEVICE)
+    enc_3.to(DEVICE)
+    enc_4.to(DEVICE)
+    enc_5.to(DEVICE)
+
+    # extract content features
+    Content4_1 = enc_4(enc_3(enc_2(enc_1(content))))
+    Content5_1 = enc_5(Content4_1)
+    # extract style features
+    Style4_1 = enc_4(enc_3(enc_2(enc_1(style))))
+    Style5_1 = enc_5(Style4_1)
+
+    return Content4_1, Content5_1, Style4_1, Style5_1
--- main.py
+++ main.py
@@ -9,10 +9,9 @@ from fastapi.datastructures import UploadFile
 from fastapi.params import File
 from starlette.responses import FileResponse, HTMLResponse
 import torch
-import torch.nn as nn
-from torchvision import transforms
 from torchvision.utils import save_image
 
+from net import *
 
 # Globals
 app = FastAPI()
@@ -23,194 +22,6 @@ OUTPUT_FOLDER = 'results'
 if not os.path.exists(OUTPUT_FOLDER):
     os.mkdir(OUTPUT_FOLDER)
 
-
-# loss functions
-def calc_mean_std(feat, eps=1e-5):
-    # eps is a small value added to the variance to avoid divide-by-zero.
-    size = feat.size()
-    assert (len(size) == 4)
-    N, C = size[:2]
-    feat_var = feat.view(N, C, -1).var(dim=2) + eps
-    feat_std = feat_var.sqrt().view(N, C, 1, 1)
-    feat_mean = feat.view(N, C, -1).mean(dim=2).view(N, C, 1, 1)
-    return feat_mean, feat_std
-
-
-def mean_variance_norm(feat):
-    size = feat.size()
-    mean, std = calc_mean_std(feat)
-    normalized_feat = (feat - mean.expand(size)) / std.expand(size)
-    return normalized_feat
-
-
-# Networks
-decoder_arch = nn.Sequential(
-    nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(512, 256, (3, 3)),
-    nn.ReLU(),
-    nn.Upsample(scale_factor=2, mode='nearest'),
-    nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(256, 256, (3, 3)),
-    nn.ReLU(),
-    nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(256, 256, (3, 3)),
-    nn.ReLU(),
-    nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(256, 256, (3, 3)),
-    nn.ReLU(),
-    nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(256, 128, (3, 3)),
-    nn.ReLU(),
-    nn.Upsample(scale_factor=2, mode='nearest'),
-    nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(128, 128, (3, 3)),
-    nn.ReLU(),
-    nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(128, 64, (3, 3)),
-    nn.ReLU(),
-    nn.Upsample(scale_factor=2, mode='nearest'),
-    nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(64, 64, (3, 3)),
-    nn.ReLU(),
-    nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(64, 3, (3, 3)),
-)
-
-
-vgg_arch = nn.Sequential(
-    nn.Conv2d(3, 3, (1, 1)),
-    nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(3, 64, (3, 3)),
-    nn.ReLU(),  # relu1-1
-    nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(64, 64, (3, 3)),
-    nn.ReLU(),  # relu1-2
-    nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
-    nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(64, 128, (3, 3)),
-    nn.ReLU(),  # relu2-1
-    nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(128, 128, (3, 3)),
-    nn.ReLU(),  # relu2-2
-    nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
-    nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(128, 256, (3, 3)),
-    nn.ReLU(),  # relu3-1
-    nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(256, 256, (3, 3)),
-    nn.ReLU(),  # relu3-2
-    nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(256, 256, (3, 3)),
-    nn.ReLU(),  # relu3-3
-    nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(256, 256, (3, 3)),
-    nn.ReLU(),  # relu3-4
-    nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
-    nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(256, 512, (3, 3)),
-    nn.ReLU(),  # relu4-1, this is the last layer used
-    nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(512, 512, (3, 3)),
-    nn.ReLU(),  # relu4-2
-    nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(512, 512, (3, 3)),
-    nn.ReLU(),  # relu4-3
-    nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(512, 512, (3, 3)),
-    nn.ReLU(),  # relu4-4
-    nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
-    nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(512, 512, (3, 3)),
-    nn.ReLU(),  # relu5-1
-    nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(512, 512, (3, 3)),
-    nn.ReLU(),  # relu5-2
-    nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(512, 512, (3, 3)),
-    nn.ReLU(),  # relu5-3
-    nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(512, 512, (3, 3)),
-    nn.ReLU()  # relu5-4
-)
-
-
-# Self attention network
-class SANet(nn.Module):
-    def __init__(self, in_dim):
-        super(SANet, self).__init__()
-        self.f = nn.Conv2d(in_dim, in_dim, (1, 1))
-        self.g = nn.Conv2d(in_dim, in_dim, (1, 1))
-        self.h = nn.Conv2d(in_dim, in_dim, (1, 1))
-        self.sm = nn.Softmax(dim=-1)
-        self.out_conv = nn.Conv2d(in_dim, in_dim, (1, 1))
-
-    def forward(self, content, style):
-        F = self.f(mean_variance_norm(content))
-        G = self.g(mean_variance_norm(style))
-        H = self.h(style)
-        b, c, h, w = F.size()
-        F = F.view(b, -1, w * h).permute(0, 2, 1)
-        b, c, h, w = G.size()
-        G = G.view(b, -1, w * h)
-        S = torch.bmm(F, G)
-        S = self.sm(S)
-        b, c, h, w = H.size()
-        H = H.view(b, -1, w * h)
-        O = torch.bmm(H, S.permute(0, 2, 1))
-        b, c, h, w = content.size()
-        O = O.view(b, c, h, w)
-        O = self.out_conv(O)
-        O += content
-        return O
-
-
-# self attention module with attention networks for relu4_1, relu5_1
-class SAModule(nn.Module):
-    def __init__(self, in_dim):
-        super(SAModule, self).__init__()
-        self.sanet4_1 = SANet(in_dim=in_dim)
-        self.sanet5_1 = SANet(in_dim=in_dim)
-        self.upsample5_1 = nn.Upsample(scale_factor=2, mode='nearest')
-        self.merge_conv_pad = nn.ReflectionPad2d((1, 1, 1, 1))
-        self.merge_conv = nn.Conv2d(in_dim, in_dim, (3, 3))
-
-    def forward(self, content4_1, style4_1, content5_1, style5_1):
-        return self.merge_conv(self.merge_conv_pad(self.sanet4_1(content4_1, style4_1) + self.upsample5_1(self.sanet5_1(content5_1, style5_1))))
-
-
-# transforms to perfom on image before using
-def test_transform():
-    transform_list = []
-    transform_list.append(transforms.Resize(512))
-    transform_list.append(transforms.ToTensor())
-    transform = transforms.Compose(transform_list)
-    return transform
-
-
-# extract style and content features
-def feat_extractor(vgg, content, style):
-    # extract used layers from vgg network
-    enc_1 = nn.Sequential(*list(vgg.children())[:4])  # input -> relu1_1
-    enc_2 = nn.Sequential(*list(vgg.children())[4:11])  # relu1_1 -> relu2_1
-    enc_3 = nn.Sequential(*list(vgg.children())[11:18])  # relu2_1 -> relu3_1
-    enc_4 = nn.Sequential(*list(vgg.children())[18:31])  # relu3_1 -> relu4_1
-    enc_5 = nn.Sequential(*list(vgg.children())[31:44])  # relu4_1 -> relu5_1
-
-    # move everything to GPU
-    enc_1.to(DEVICE)
-    enc_2.to(DEVICE)
-    enc_3.to(DEVICE)
-    enc_4.to(DEVICE)
-    enc_5.to(DEVICE)
-
-    # extract content features
-    Content4_1 = enc_4(enc_3(enc_2(enc_1(content))))
-    Content5_1 = enc_5(Content4_1)
-    # extract style features
-    Style4_1 = enc_4(enc_3(enc_2(enc_1(style))))
-    Style5_1 = enc_5(Style4_1)
-
-    return Content4_1, Content5_1, Style4_1, Style5_1
 
 
 # perform style transfer
@@ -223,7 +34,7 @@ def style_transfer(vgg, decoder, sa_module, content, style, alpha=1):
 
     # get features for both style and content
     Content4_1, Content5_1, Style4_1, Style5_1 = feat_extractor(
-        vgg, content, style)
+        vgg, content, style, DEVICE)
 
     # get content features importance (used to select how much of content to keep according to alpha)
     Fccc = sa_module(Content4_1, Content4_1, Content5_1, Content5_1)
